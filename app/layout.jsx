aae65e131dc039030dbbfef2dec2c1cf91eae7ef
Alyssa Voccia <avoccia@hotmail.com>
2024-04-04 09:06:32 -0400

    Added hero, dynamic info box components, and footer

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import '@/assets/styles/globals.css';
 import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
 
 export const metadata = {
   title: 'PropertyPulse | Find The Perfect Rental',
@@ -10,9 +11,10 @@ export const metadata = {
 const MainLayout = ({ children }) => {
   return (
     <html lang="en">
-      <body>
+      <body className='flex flex-col h-screen'>
         <Navbar />
         <main>{children}</main>
+        <Footer />
       </body>
     </html>
   )
