297b476733715fd824550f5c121b383191103743
Alyssa Voccia <avoccia@hotmail.com>
2024-04-11 08:48:09 -0400

    Added session provider wrapper

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
-import '@/assets/styles/globals.css';
+import AuthProvider from '@/components/AuthProvider';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
+import '@/assets/styles/globals.css';
 
 export const metadata = {
   title: 'PropertyPulse | Find The Perfect Rental',
@@ -10,13 +11,15 @@ export const metadata = {
 
 const MainLayout = ({ children }) => {
   return (
-    <html lang="en">
-      <body className='flex flex-col h-screen'>
-        <Navbar />
-        <main>{children}</main>
-        <Footer />
-      </body>
-    </html>
+    <AuthProvider>
+      <html lang="en">
+        <body className='flex flex-col h-screen'>
+          <Navbar />
+          <main>{children}</main>
+          <Footer />
+        </body>
+      </html>
+    </AuthProvider>
   )
 }
 
